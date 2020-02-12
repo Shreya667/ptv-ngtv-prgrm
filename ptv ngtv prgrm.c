d2c0b8fba4e69f29a761a19ec73fea009ed844f3
shreya667 <yshreya056@gmail.com>
2020-02-12 22:33:53 +0530

    meri file

---
+++ ptv ngtv prgrm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num>0)
+    {
+     printf("number is positive");
+    }
+    else{
+        printf("number is negative");
+
+    }
+    return 0;
+}
